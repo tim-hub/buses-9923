c1e32d98f5abf2b16f8ed3b82ada56730e315733
tim-hub <tim_w@outlook.com>
2019-05-17 11:18:02 +1200

    refactor logger to an immutable way

--- logic/logger.js
+++ logic/logger.js
@@ -1,14 +1,22 @@
 class Logger {
   constructor() {
-      this.logs = [];
+      this._logs = [];
+  }
+  get logs(){
+    const logs = [...this._logs];
+    return logs;
   }
   log(message) {
-    this.log('logging', message)
+    this.log('logging', message);
   }
   log(msg_level, message) {
     const timestamp = new Date().toISOString();
-    this.logs.push({ timestamp, msg_level, message });
+    this._logs=[...this._logs, { timestamp, msg_level, message }]
     console.log(`${timestamp} - ${msg_level} - ${message}`);
+  }
+  latest(){
+    const last_log = Object.assign({},this._logs[this._logs.length-1]);
+    return last_log;
   }
 }
 export default new Logger();
