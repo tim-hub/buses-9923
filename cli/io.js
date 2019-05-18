0b9f99cd12d6785a3be2acf5daaaebd7a6a6ced9
tim-hub <tim_w@outlook.com>
2019-05-18 22:14:52 +1200

    update logger to control msg_types displayed

--- cli/io.js
+++ cli/io.js
@@ -32,11 +32,11 @@ const runCommands = (file_path)=>{
       let [x,y,facing]= ((cmd)=>{
         return cmd.split(/[ ,]+/).filter(v=> v!=='PLACE');
       })(command.toUpperCase());
-      logger.log('Input', `${command} - Place to (${x},${y}) facing to ${facing}`)
+      logger.log('io', `${command} - Place to (${x},${y}) facing to ${facing}`)
       the_park.place(x,y,facing);
     }else if(the_park.getLatestBus()=== null || the_park.getLatestBus()=== undefined){
       // this will ignore all commands before a proper PLACE
-      logger.log('Input', command+' is discarded, because valid PLACE command is not run yet.');
+      logger.log('io', command+' is discarded, because valid PLACE command is not run yet.');
     }else if(command.toUpperCase() === 'LEFT'){
       // turn the bus left
       the_park.left();
@@ -45,12 +45,12 @@ const runCommands = (file_path)=>{
       the_park.right();
     }else if(command.toUpperCase() === 'REPORT'){
       // report the bus current position
-      logger.log('Input', the_park.getLatestBus());
+      logger.log('io', the_park.getLatestBus());
       outputs.push(the_park.getLatestBus().toString());
       console.log(outputs[outputs.length-1]);
     }else if(command.toUpperCase() === 'MOVE'){
       // move the bus towards the current facing direction
-      logger.log('Input', 'Move the bus');
+      logger.log('io', 'Move the bus');
       the_park.move();
     }
   });
@@ -59,7 +59,7 @@ const runCommands = (file_path)=>{
 
 /**
  * Read commands from the file
- * @param {string} the_file - The path of the input file(utf8), commands are required to be separated by new line
+ * @param {string} the_file - The path of the file(utf8), commands are required to be separated by new line
  */
 export const readCommands = (the_file)=>{
   /**
