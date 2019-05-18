6aa0dbd78448d78e5cadd98b0df5f01ce7af741f
tim-hub <tim_w@outlook.com>
2019-05-18 21:55:38 +1200

    add comments and return the results

--- cli/io.js
+++ cli/io.js
@@ -3,9 +3,10 @@ import path from 'path';
 import logger from '../logger';
 import {the_park} from '../logic/park';
 
-
+/**
+ * The Regex for the proper PLACE command
+ */
 const placeReg = new RegExp('PLACE.[0-9](,)[0-9](,)(NORTH|WEST|EAST|SOUTH)');
-
 
 /**
  * Return commands read from the file path
@@ -13,13 +14,19 @@ const placeReg = new RegExp('PLACE.[0-9](,)[0-9](,)(NORTH|WEST|EAST|SOUTH)');
  */
 const runCommands = (file_path)=>{
   const commands = []
+
+  /**
+   * Read the file line by line
+   */
   fs.readFileSync(file_path, 'utf-8').split(/\r?\n/).forEach((line)=>{
     commands.push(line);
   })
 
+  const outputs = [];
+
   commands
   .filter(cmd=>cmd!=='')
-  .map((command, i)=>{
+  .map((command)=>{
     if(command.toUpperCase().search(placeReg)===0){
       // A valid PLACE command
       let [x,y,facing]= ((cmd)=>{
@@ -31,24 +38,33 @@ const runCommands = (file_path)=>{
       // this will ignore all commands before a proper PLACE
       logger.log('Input', command+' is discarded, because valid PLACE command is not run yet.');
     }else if(command.toUpperCase() === 'LEFT'){
+      // turn the bus left
       the_park.left();
     }else if(command.toUpperCase() === 'RIGHT'){
+      // turn the bus right
       the_park.right();
     }else if(command.toUpperCase() === 'REPORT'){
+      // report the bus current position
       logger.log('Input', the_park.getLatestBus());
-      console.log(the_park.getLatestBus().toString());
+      outputs.push(the_park.getLatestBus().toString());
+      console.log(outputs[outputs.length-1]);
     }else if(command.toUpperCase() === 'MOVE'){
+      // move the bus towards the current facing direction
       logger.log('Input', 'Move the bus');
       the_park.move();
     }
   });
+  return [...outputs];
 }
 
 /**
- *
+ * Read commands from the file
  * @param {string} the_file - The path of the input file(utf8), commands are required to be separated by new line
  */
 export const readCommands = (the_file)=>{
+  /**
+   * Check the input whether it is a relative path or not
+   */
   if (!path.isAbsolute(the_file)){
     the_file = path.join(__dirname, the_file);
   }
@@ -57,8 +73,6 @@ export const readCommands = (the_file)=>{
    */
   fs.access(the_file, fs.F_OK, (err) => {
     if (err) throw err;
-    runCommands(the_file);
+    return runCommands(the_file);
   })
-
-
 }
